c2f1bb5bd764b80e63f52f269ce2921582eddccb
Magnus Junghard Huttu <magnushuttu@gmail.com>
2025-01-25 16:55:18 +0100

    add vercel fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://happypanda.ai",
-  integrations: [mdx(), sitemap(), tailwind()],
-  base: "/blog",
+  output: "static",
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  adapter: vercel({}),
+  base: "/blog", // This ensures assets are loaded correctly
 });
